51cf9e8ad8dfdc75ac68f5d2de451031c0e347f9
Tom Greuter <tom@Toms-MacBook-Pro.local>
2017-06-06 17:24:12 +0200

    Add Sass

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@ var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
 var gls = require( 'gulp-live-server' );
+var sass = require( 'gulp-sass' );
 var dist = 'dist';
 
 var paths = {
   scripts: ['src/js/**/*.js'],
   templates: ['src/*.html'],
+  css: ['src/scss/*.scss'],
   images: 'src/img/**/*'
 };
 
@@ -55,12 +57,20 @@ gulp.task( 'templates', function() {
         .pipe( gulp.dest( dist ) );
 } );
 
+// PRocess SASS
+gulp.task( 'sass', function() {
+    return gulp.src( './src/scss/**/*.scss' )
+        .pipe( sass.sync().on( 'error', sass.logError ) )
+        .pipe( gulp.dest( dist ) );
+} );
+
 // Rerun the task when a file changes
 gulp.task('watch', function() {
   gulp.watch(paths.scripts, ['scripts']);
   gulp.watch(paths.images, ['images']);
   gulp.watch(paths.templates, ['templates']);
+  gulp.watch(paths.css, ['sass']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['clean', 'templates', 'scripts', 'images', 'watch']);
+gulp.task('default', ['clean', 'templates', 'sass', 'scripts', 'images', 'watch']);
